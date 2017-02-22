e8b6e1fa0dffd916694e67acf79e318f6c3b2c81
luoxiansong <xsluo@126.com>
2017-02-22 09:05:58 +0800

    显示基本地图

--- jianmeng/AppDelegate.m
+++ jianmeng/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <QMapKit/QMapKit.h>
 
 @interface AppDelegate ()
 
@@ -14,9 +15,9 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [QMapServices sharedServices].apiKey = @"BEIBZ-TN7CX-QMF4W-ZO5OB-IXYIQ-E7FZN";
     return YES;
 }
 
@@ -24,7 +25,7 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
+   }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
--- jianmeng/ViewController.m
+++ jianmeng/ViewController.m
@@ -10,10 +10,12 @@
 #import "MenuView.h"
 #import "LeftMenuView.h"
 #import "configTableViewController.h"
+#import <QMapKit/QMapKit.h>
 
-@interface ViewController ()<HomeMenuViewDelegate>
+@interface ViewController ()<HomeMenuViewDelegate,UIGestureRecognizerDelegate>
 
 @property (nonatomic ,strong)MenuView   * menu;
+@property (nonatomic,strong) QMapView *mapView;
 
 @end
 
@@ -23,13 +25,11 @@
     [super viewDidLoad];
     
     [self.view setBackgroundColor:[UIColor lightGrayColor]];
-    
-    
     LeftMenuView *demo = [[LeftMenuView alloc]initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width * 0.8, [[UIScreen mainScreen] bounds].size.height)];
     demo.customDelegate = self;
-    
     self.menu = [[MenuView alloc]initWithDependencyView:self.view MenuView:demo isShowCoverView:YES];
     
+    [self initMap];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,7 +40,7 @@
 -(void)LeftMenuViewClick:(NSInteger)tag{
     [self.menu hidenWithAnimation];
     
-    NSLog(@"tag = %lu",tag);
+    //NSLog(@"tag = %lu",tag);
     
     if (tag == 3) {
         [self performSegueWithIdentifier:@"toConfig" sender:self];
@@ -49,6 +49,40 @@
 
 - (IBAction)leftNavAction:(id)sender {
     [self.menu show];
+}
+
+- (void)initMap {
+    _mapView = [[QMapView alloc]
+                initWithFrame: CGRectMake(0,
+                                          CGRectGetMaxY(self.navigationController.navigationBar.frame),
+                                          CGRectGetWidth(self.view.frame),
+                                          CGRectGetHeight(self.view.frame) - CGRectGetMaxY(self.navigationController.navigationBar.frame))];
+    [self.view addSubview:self.mapView];
+    //初始化设置地图中心点坐标需要异步加入到主队列
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [_mapView setCenterCoordinate:CLLocationCoordinate2DMake(39.963438, 116.316376)
+                            zoomLevel:12.01
+                             animated:NO];
+    });
+    //由于zoomLevel的调用区间是左闭右开的，在调用某一级别的图片时，
+    //实际调用的是上级的最大缩放级别，底图有可能会看到锯齿，
+    //此时可以在缩放级别上加上0.01使用高级别底图
+    [_mapView setZoomLevel:12.01];
+    
+    //添加手势识别
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc]
+                                                 initWithTarget:self
+                                                 action:@selector(gestureAction:)];
+    [gestureRecognizer setDelegate:self];
+    [_mapView addGestureRecognizer:gestureRecognizer];
+}
+-(void)gestureAction:(UIGestureRecognizer *)gestureRecognizer {
+    CGPoint point = [gestureRecognizer locationOfTouch:0 inView:_mapView];
+    NSLog(@"Tap at:%f,%f", point.x, point.y);
+}
+
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
 }
 
 @end
